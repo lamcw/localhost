8e590075fa7cb31150f83dd6e0ee42cc3298f5ea
lamcw <thomas@lamcw.com>
2018-08-18 00:10:26 +1000

    Moved apps to one folder and added `auth` app.
    Signed-off-by: lamcw <thomas@lamcw.com>

--- placeholder/settings.py
+++ placeholder/settings.py
@@ -31,7 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'core.apps.CoreConfig',
+    'placeholder.core',
+    'placeholder.auth',
+    'placeholder.messaging',
+    'placeholder.report',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
