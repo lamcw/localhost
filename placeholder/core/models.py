47b23597b11a87cefb93e9c7689ab85a089099a9
lamcw <thomas@lamcw.com>
2018-08-18 20:28:15 +1000

    Added core models.
    Signed-off-by: lamcw <thomas@lamcw.com>

--- placeholder/core/models.py
+++ placeholder/core/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 
+from placeholder.authentication.models import User
+
 
 class Listing(models.Model):
-    host = models.ForeignKey('User')
+    host = models.ForeignKey(User, on_delete=models.PROTECT)
     title = models.CharField(max_length=70)
     description = models.TextField(max_length=200)
     # latitude = TODO
     # longitude = TODO
 
+    def __str__(self):
+        return f"Listing: {self.title} by {self.host}"
+
 
 class TTB(models.Model):
-    listing = models.ForeignKey('Listing', on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
     BED = 'B'
     ROOM = 'R'
     PROPERTY = 'P'
@@ -20,17 +25,49 @@ class TTB(models.Model):
         (PROPERTY, 'Entire Property'),
     )
     ttb_type = models.CharField(
-        max_length=1,
-        choices=TYPE_CHOICES,
-        default=ROOM
-    )
+        max_length=1, choices=TYPE_CHOICES, default=ROOM)
     price = models.DecimalField()
     buyout_price = models.DecimalField()
     rating = models.IntegerField(choices=[(i, i) for i in range(6)])
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length=40)
     description = models.TextField(max_length=200)
     highest_bidder = models.OneToOneField(User)
 
 
-class TTBImage(models.Model):
-    pass
+class Amenity(models.Model):
+    ttb = models.ManyToManyField(TTB)
+    item = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"Amenity: {self.items}"
+
+
+class Image(models.Model):
+    img = models.ImageField()
+
+    class Meta:
+        abstract = True
+
+
+class TTBImage(Image):
+    ttb = models.OneToOneField(TTB)
+
+
+class ListingImage(Image):
+    listing = models.OneToOneField(Listing)
+
+
+class Booking(models.Model):
+    user = models.OneToOneField(User)
+    ttb = models.ForeignKey(TTB, on_delete=models.PROTECT)
+    price = models.DecimalField()
+    checkin_date = models.DateField()
+    checkout_date = models.DateField()
+    CART = 'C'
+    PAID = 'P'
+    STATUS_CHOCIE = (
+        (CART, 'Cart'),
+        (PAID, 'Paid'),
+    )
+    status = models.CharField(
+        max_length=1, choices=STATUS_CHOCIE, default=CART)
