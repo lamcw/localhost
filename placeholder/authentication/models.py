f002481ad1b5f63ec5792f8a1af316e0f64bace4
lamcw <thomas@lamcw.com>
2018-08-23 12:45:40 +1000

    User model fix.
    - made description optional
    - DecimalField for credits
    
    Signed-off-by: lamcw <thomas@lamcw.com>

--- placeholder/authentication/models.py
+++ placeholder/authentication/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+from decimal import Decimal
+
 
 class User(models.Model):
     MALE = 'M'
@@ -10,7 +12,8 @@ class User(models.Model):
         (FEMALE, 'Female'),
     )
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=100, null=True, blank=True)
     dob = models.DateField()
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-    credits = models.IntegerField()
+    credits = models.DecimalField(
+        max_digits=8, decimal_places=2, default=Decimal('0.0'))
