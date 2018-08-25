1580a0ac4dc1861c79bddc80339ca2f7b2ce5b48
lamcw <thomas@lamcw.com>
2018-08-25 13:06:32 +1000

    Admin interface for PropertyItems.
    Signed-off-by: lamcw <thomas@lamcw.com>

--- placeholder/core/models.py
+++ placeholder/core/models.py
@@ -2,53 +2,56 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from polymorphic.models import PolymorphicModel
+from polymorphic.showfields import ShowFieldType
 
 
-class PropertyItem(PolymorphicModel):
+class PropertyItem(ShowFieldType, PolymorphicModel):
     price = models.DecimalField(_('price'), max_digits=8, decimal_places=2)
     buyout_price = models.DecimalField(
         _('buyout price'),
         max_digits=8,
         decimal_places=2,
-        help_text=_('buyout price during auction')
+        help_text=_('buyout price during auction.')
     )
     title = models.CharField(_('title'), max_length=40)
     description = models.TextField(_('description'), max_length=200)
     highest_bidder = models.OneToOneField(
         get_user_model(),
         null=True,
-        on_delete=models.SET_NULL
+        on_delete=models.SET_NULL,
     )
     open_for_auction = models.BooleanField(default=False)
 
 
 class Property(PropertyItem):
-    host = models.ForeignKey(
-        get_user_model(),
-        on_delete=models.PROTECT,
-        related_name='%(class)s'
-    )
+    host = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     latitude = models.DecimalField(max_digits=10, decimal_places=7)
     longitude = models.DecimalField(max_digits=10, decimal_places=7)
 
     def __str__(self):
-        return f"Listing: {self.title} by {self.host}"
+        return f"Property: {self.title} owned by {self.host}"
 
 
 class Room(PropertyItem):
     property_ptr = models.ForeignKey(Property, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"Room in {self.property_ptr}"
+
 
 class Bed(PropertyItem):
     room_ptr = models.ForeignKey(Room, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"Bed in {self.room_ptr}"
+
 
 class Amenity(models.Model):
-    property_item = models.ManyToManyField(Room)
+    property_item = models.ManyToManyField(PropertyItem)
     item = models.CharField(max_length=15)
 
     def __str__(self):
-        return f"Amenity: {self.items}"
+        return f"Amenity {self.items}"
 
 
 class PropertyItemImage(models.Model):
@@ -72,7 +75,7 @@ class Booking(models.Model):
         max_length=1, choices=STATUS_CHOCIE, default=CART)
 
 
-class Review(models.Model):
+class Review(PolymorphicModel):
     rating = models.IntegerField(choices=[(i, i) for i in range(6)])
     description = models.TextField(_('description'))
 
--- placeholder/report/models.py
+++ placeholder/report/models.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from polymorphic.models import PolymorphicModel
 
 from placeholder.core.models import PropertyItem
 
 
-class Report(models.Model):
+class Report(PolymorphicModel):
     from_user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     description = models.TextField(_('description'))
     time = models.DateTimeField(auto_now_add=True)
