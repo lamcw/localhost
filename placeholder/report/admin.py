b32576f7325cece02112d6770b6fa1d4d897c8cf
lamcw <thomas@lamcw.com>
2018-08-27 16:54:13 +1000

    Added reports in admin panel.
    Signed-off-by: lamcw <thomas@lamcw.com>

--- placeholder/report/admin.py
+++ placeholder/report/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from polymorphic.admin import (PolymorphicChildModelAdmin,
+                               PolymorphicParentModelAdmin)
 
-# Register your models here.
+from placeholder.report.models import PropertyReport, Report, UserReport
+
+
+class ReportChildAdmin(PolymorphicChildModelAdmin):
+    base_model = Report
+    exclude = ('time', )
+
+
+@admin.register(UserReport)
+class UserReportAdmin(ReportChildAdmin):
+    base_model = UserReport
+
+
+@admin.register(PropertyReport)
+class PropertyReportAdmin(ReportChildAdmin):
+    base_model = PropertyReport
+
+
+@admin.register(Report)
+class ReportParentAdmin(PolymorphicParentModelAdmin):
+    base_model = Report
+    child_models = (UserReport, PropertyReport)
