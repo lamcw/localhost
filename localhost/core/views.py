66ff9537c081a7ba31464b31cd597c01f181db45
Edward <eddai741@gmail.com>
2018-09-12 14:12:46 +1000

    removed search radius, added order by closest distance

--- localhost/core/views.py
+++ localhost/core/views.py
@@ -1,6 +1,6 @@
 import math
 
-from django.db.models import F, Q
+from django.db.models import F, Q, Case, When
 from django.views.generic import DetailView, ListView
 from geopy import distance, units
 
@@ -14,51 +14,49 @@ class PropertyDetailView(DetailView):
 class SearchResultsView(ListView):
     model = Property
     template_name = 'core/search_results.html'
+    paginate_by = 20
 
     def get_queryset(self, **kwargs):
         queryset = super(SearchResultsView, self).get_queryset()
 
         # time = datetime.datetime.now()
         time = '14:00:00'
-        latitude = float(self.request.GET.get('lat'))
-        longitude = float(self.request.GET.get('long'))
-        search_range = float(self.request.GET.get('range', 10))
+        latitude = float(self.request.GET.get('lat',-33.9666))
+        longitude = float(self.request.GET.get('long',151.1))
         guests = int(self.request.GET.get('guests', 1))
         bid_now = int(self.request.GET.get('bidnow', 0))
         checkin = self.request.GET.get('checkin', time)
-        # "Box" range filter to narrow queries
-        latitude_offset = units.degrees(
-            arcminutes=units.nautical(kilometers=search_range))
-        longitude_offset = latitude_offset / math.cos(math.radians(latitude))
-        queryset = queryset.filter(
-            latitude__range=(latitude - latitude_offset,
-                             latitude + latitude_offset),
-            longitude__range=(longitude - longitude_offset,
-                              longitude + longitude_offset))
 
         if bid_now:
             q1 = queryset.filter(
                 Q(earliest_checkin_time__lt=F('latest_checkin_time')),
                 Q(earliest_checkin_time__lt=checkin),
-                latest_checkin_time__gt=checkin)
+                latest_checkin_time__gt=checkin
+            )
 
             q2 = queryset.filter(
                 Q(earliest_checkin_time__gt=F('latest_checkin_time')),
                 Q(earliest_checkin_time__lt=checkin)
-                | Q(latest_checkin_time__gt=checkin))
+                | Q(latest_checkin_time__gt=checkin)
+            )
+
             queryset = q1 | q2
 
             queryset = queryset.filter(
                 session__end_time__gt=time,
                 session__start_time__lt=time,
                 property_item__available=True,
-                property_item__capacity__gte=guests).distinct()
+                property_item__capacity__gte=guests
+            ).distinct()
 
-        properties = []
+        properties = list()
         for p in queryset:
             geodesic_distance = distance.distance(
                 (latitude, longitude), (p.latitude, p.longitude)).kilometers
-            if geodesic_distance < search_range:
-                properties.append(p)
+            properties.append(tuple((p.id, geodesic_distance)))
 
-        return queryset.filter(id__in=[p.id for p in properties])
+        sorted_properties = sorted(properties, key=lambda x: x[1])
+        sorted_ids = list(i[0] for i in sorted_properties)
+        preserved = Case(*[When(pk=pk, then=pos) for pos, pk in enumerate(sorted_ids)])
+
+        return queryset.filter(pk__in=sorted_ids).order_by(preserved)
