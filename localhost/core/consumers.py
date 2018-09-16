eae5f8c9b061b2ae94f177a562bd8fd45716134e
Edward <eddai741@gmail.com>
2018-09-16 14:02:14 +1000

    added bid table for bidding

--- localhost/core/consumers.py
+++ localhost/core/consumers.py
@@ -1,12 +1,12 @@
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from localhost.core.models import PropertyItem
 import json
 
-class ChatConsumer(WebsocketConsumer):
+class BidConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
@@ -26,19 +26,19 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        print(message)
-
+        propertyitem = PropertyItem.objects.get(pk=self.room_name)
+        print(propertyitem)
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
-                'type': 'chat_message',
+                'type': 'bid',
                 'message': message
             }
         )
 
     # Receive message from room group
-    def chat_message(self, event):
+    def bid(self, event):
         message = event['message']
 
         # Send message to WebSocket
--- localhost/core/routing.py
+++ localhost/core/routing.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import consumers
 
 websocket_urlpatterns = [
-    url(r'^ws/property-detail/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
+    url(r'^ws/property-detail/(?P<room_name>[^/]+)/$', consumers.BidConsumer),
 ]
