8abeb84470e438e568e4ad0bafa588a6058a9c29
lamcw <thomas@lamcw.com>
2018-09-18 20:26:24 +1000

    configs for production environment
    Signed-off-by: lamcw <thomas@lamcw.com>

--- localhost/settings_production.py
+++ localhost/settings_production.py
@@ -2,5 +2,23 @@ import os
 
 from localhost.settings import *
 
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '54.206.61.115']
+
+# Security
+# >= 50 characters & > 5 unique characters
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
 DEBUG = False
+
+# prevent clickjacking
+X_FRAME_OPTIONS = 'DENY'
+
+# Setup HTTPS before using the below options
+# CSRF_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
+# SECURE_HSTS_SECONDS = 3600
+# SECURE_SSL_REDIRECT = True
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
