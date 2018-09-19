8f6d7a2bcbee3526637c9de288c7c23402b16555
lamcw <thomas@lamcw.com>
2018-09-19 12:22:14 +1000

    fixed settings path
    Signed-off-by: lamcw <thomas@lamcw.com>

--- localhost/wsgi.py
+++ localhost/wsgi.py
@@ -11,8 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-SETTINGS = 'localhost.settings' if os.environ.get(
-    'SECRET_KEY') else 'localhost.settings_dev'
+SETTINGS = 'localhost.settings_production' if os.environ.get(
+    'SECRET_KEY') else 'localhost.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', SETTINGS)
 
 application = get_wsgi_application()
