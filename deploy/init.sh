41a5a41334de680a07debef5930a935b9534af19
jtalowell <jtalowell@protonmail.com>
2018-09-20 04:45:23 +0000

    Fixing venv bug in server init script

--- deploy/init.sh
+++ deploy/init.sh
@@ -9,9 +9,11 @@ sudo /etc/init.d/nginx stop
 sudo /etc/init.d/nginx start
 
 cd ..
+source venv/bin/activate
 tput setaf 6; echo "Restarting uwsgi..."
 while $(killall uwsgi &> /dev/null); do
     sleep 1
 done
 SECRET_KEY="$1" DB_USER="$2" DB_PW="$3" uwsgi --socket :8001 --wsgi-file localhost/wsgi.py &> /tmp/uwsgi.log &
+exit
 tput setaf 6; echo "Finished...exiting."
