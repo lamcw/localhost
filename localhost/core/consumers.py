8b9819777d82597316b51319ca2330af96c24a7e
jtalowell <jtalowell@protonmail.com>
2018-09-23 22:39:29 +1000

    Baseplate for socket role multiplexing

--- localhost/core/consumers.py
+++ localhost/core/consumers.py
@@ -1,114 +1,85 @@
-import json
-
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
-from django.utils import timezone
-from localhost.core.models import Bid, PropertyItem, BiddingSession
-
-OK = 0
-ERROR_INVALID_BID = -1
-ERROR_INVALID_SESSION = -2
-ERROR_INSUFFICIENT_FUNDS = -3
+from channels.generic.websocket import JsonWebsocketConsumer
 
 
-class BidConsumer(WebsocketConsumer):
-    def connect(self):
-        pk = self.scope['url_route']['kwargs']['item_id']
-        self.room_group_name = 'bidding_%s' % pk
-        self.property_item = PropertyItem.objects.get(pk=pk)
-        self.user = self.scope['user']
+class MultiplexJsonWebsocketConsumer(JsonWebsocketConsumer):
+    """
+    Variant of JsonWebsocketConsumer that is extended to support group
+    multiplexing; allowing consumers to participate in multiple groups
+    with a single socker connection.
+    """
 
-        # Join room group
+    def disconnect(self, code):
+        for group in self.groups:
+            self.unsubscribe(group)
+
+    def subscribe(self, group):
+        """
+        Subscribes the consumer to a group.
+        """
+        self.groups.append(group)
         async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
+            group,
             self.channel_name
         )
 
-        self.accept()
-
-    def disconnect(self, close_code):
+    def unsubscribe(self, group):
         """
-        Leave room group.
+        Unsubscribes the consumer from a group.
         """
         async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name,
+            group,
             self.channel_name
         )
 
-    def receive(self, text_data):
+    def is_subscribed(self, group):
         """
-        Receive message from WebSocket.
+        Checks if the consumer is subscribed to a group.
         """
-        text_data_json = json.loads(text_data)
-        user_bid = text_data_json['message']
-        time_now = timezone.localtime().time()
+        return group in self.groups
 
-        try:
-            min_next_bid = Bid.objects.filter(
-                property_item=self.property_item). \
-                latest('amount').amount + 1
-        except Bid.DoesNotExist:
-            min_next_bid = self.property_item.min_price
 
-        current_session = BiddingSession.objects.filter(
-                propertyitem=self.property_item,
-                end_time__gt=time_now,
-                start_time__lte=time_now)
+class Consumer(MultiplexJsonWebsocketConsumer):
+    """
+    Generalised variant of MultiplexJsonWebsocketConsumer for use by all
+    socket connections.
+    """
 
-        if not current_session.exists():
-            self.send(text_data=json.dumps({
-                'type' : 'BID_RESPONSE',
-                'status_code' : ERROR_INVALID_SESSION,
-                'content': 'ERROR_INVALID_SESSION'
-            }))
-
-        elif user_bid > self.user.credits:
-            self.send(text_data=json.dumps({
-                'type' : 'BID_RESPONSE',
-                'status_code' : ERROR_INSUFFICIENT_FUNDS,
-                'content': 'ERROR_INSUFFICIENT_FUNDS'
-            }))
-
-        elif user_bid < min_next_bid:
-            self.send(text_data=json.dumps({
-                'type' : 'BID_RESPONSE',
-                'status_code' : ERROR_INVALID_BID,
-                'content': 'ERROR_INVALID_BID'
-            }))
+    def connect(self):
+        if self.scope['user'].is_authenticated:
+            self.accept()
         else:
-            Bid.objects.create(
-                property_item=self.property_item,
-                bidder=self.user,
-                amount=user_bid)
+            self.close()
 
-            async_to_sync(self.channel_layer.group_send)(
-                self.room_group_name,
-                {
-                    'type' : 'bid',
-                    'amount': user_bid,
-                    'user_id' : self.user.id,
-                    'user_name' : self.user.first_name
-                }
-            )
-            self.send(text_data=json.dumps({
-                'type' : 'BID_RESPONSE',
-                'status_code' : OK,
-                'content': 'SUCCESSFUL_BID'
-            }))
+    def receive_json(self, content, **kwargs):
+        try:
+            req = content['request']
+        except KeyError:
+            pass
 
+        if req == 'subscribe':
+            try:
+                group = content['data']['group']
+            except KeyError:
+                pass
+            self.request_subscribe(group)
+        elif req == 'unsubscribe':
+            try:
+                group = content['data']['group']
+            except KeyError:
+                pass
+            self.request_unsubscribe(group)
 
+    def request_subscribe(self, group):
+        """
+        Handles a request to subscribe to a group
+        """
+        if not self.is_subscribed(group):
+            self.subscribe(group)
 
-    def bid(self, event):
-        amount = event['amount']
-        user_id = event['user_id']
-        user_name = event['user_name']
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({
-            'type' : 'BID_GLOBAL',
-            'status_code' : '0',
-            'content': {
-                'user_id' : user_id,
-                'user_name' : user_name,
-                'amount' : amount,
-            }
-        }))
+    def request_unsubscribe(self, group):
+        """
+        Handles a request to unsubscribe to a group
+        """
+        if self.is_subscribed(group):
+            self.unsubscribe(group)
