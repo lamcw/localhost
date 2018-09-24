fa201d493bf7b3d58f9e73eea52ba505705a094e
jtalowell <jtalowell@protonmail.com>
2018-09-24 14:49:29 +1000

    Adding client socket class with helper functions

--- localhost/core/static/core/js/msocket/msocket.js
+++ localhost/core/static/core/js/msocket/msocket.js
@@ -3,7 +3,9 @@
  * @author Jasper Lowell / @jtalowell
  * @date 24 Sep 2018
  * @brief File containing library interface and implementation for cooperative
- *        multiplex sockets.
+ *        multiplex sockets. An additional client-server protocol helper class
+ *        is also provided to manage general tasks such as placing a bid and
+ *        subscribing to new groups.
  *
  * As the name 'cooperative multiplex sockets' implies, this library requires
  * a cooperative server. The protocol used is simple:
@@ -21,6 +23,7 @@
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API
  */
+
 
 class MSocket {
   /**
@@ -159,5 +162,65 @@ class MSocket {
     } else {
       this.socket.send(message);
     }
+  }
+}
+
+
+class ClientSocket extends MSocket {
+  /**
+   * @brief Subscribes to a group
+   *        Further messages to the group will be sent to the client socket
+   *
+   * @param group_class ['property_item', 'conversation']
+   * @param group       The ID that together with the class identifies a unique group
+   * */
+  subscribe(group_class, group) {
+    var payload = {
+      'group': group_class + '_' + group
+    };
+    this.send('subscribe', payload);
+  }
+
+  /**
+   * @brief Unsubscribes from a group
+   *
+   * @param group_class ['property_item', 'conversation']
+   * @param group       The ID that together with the class identifies a
+   *                    unique group
+   * */
+  unsubscribe(group_class, group) {
+    var payload = {
+      'group': group_class + '_' + group
+    };
+    this.send('unsubscribe', payload);
+  }
+
+  /**
+   * @brief Bids on a property specified by its id
+   *
+   * @param property_item_id The id of the property item the bid is for
+   * @param amount           The bid amount
+   * */
+  bid(property_item_id, amount) {
+    var payload = {
+      'property_item_id': property_item_id,
+      'amount': amount
+    };
+    this.send('bid', payload);
+  }
+
+  /**
+   * @brief Sends a message to a conversation
+   *
+   * @param conversation_id The id of the conversation the message should be
+   *                        sent to
+   * @param message         The message to be sent
+   * */
+  message(conversation_id, message) {
+    var payload = {
+      'conversation_id': conversation_id,
+      'message': message
+    };
+    this.send('message', payload);
   }
 }
