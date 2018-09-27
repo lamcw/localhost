6e492f28af437779baf6abce64328470fedbdc40
lamcw <thomas@lamcw.com>
2018-09-27 12:56:02 +1000

    Added celery to project.
    **Note** Celery won't work on Python 3.7
    see https://github.com/celery/celery/issues/4500
    
    Signed-off-by: lamcw <thomas@lamcw.com>

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(
         'django>=2.1.1', 'psycopg2-binary>=2.7.5', 'Pillow>=5.2.0',
         'django-polymorphic>=2.0.3', 'django-widget-tweaks>=1.4.3',
         'googlemaps>=3.0.2', 'pyyaml>=3.13', 'uWSGI>=2.0.17.1',
-        'channels>=2.1.3', 'channels-redis>=2.3.0', 'factory_boy>=2.11.1'
+        'channels>=2.1.3', 'channels-redis>=2.3.0', 'factory_boy>=2.11.1',
+        'celery[redis]'
     ],
     packages=find_packages(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
