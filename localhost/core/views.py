87c0e17b1ee9149e939fc245c2f303d05acaf475
Edward <eddai741@gmail.com>
2018-10-02 21:15:20 +1000

    Ordered and structure message context data in messaging.html

--- localhost/core/views.py
+++ localhost/core/views.py
@@ -132,13 +132,31 @@ class MessagingView(ListView):
     template_name = 'core/messaging.html'
     model = Message
 
-    def get_queryset(self, **kwargs):
-        self.request.GET.get('recipient')
-        queryset = Message.objects.filter(
-                Q(sender=self.request.user.id) | Q(recipient=self.request.user.id))
-        return queryset
+    # def get_queryset(self, **kwargs):
+    #     queryset = Message.objects.filter(
+    #             Q(sender=self.request.user.id) | Q(recipient=self.request.user.id))
+    #     return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['recipient'] = self.request.GET.get('recipient')
+        contact_list = []
+        message_list = []
+        queryset = Message.objects.filter(
+                Q(sender=self.request.user.id) | Q(recipient=self.request.user.id))
+
+        for message in queryset:
+            if message.sender != self.request.user and message.sender not in contact_list:
+                contact_list.append(message.sender)
+            elif message.recipient != self.request.user and message.recipient not in contact_list:
+                contact_list.append(message.recipient)
+        contact_list.reverse()
+        for user in contact_list:
+            conversation = []
+            for message in queryset:
+                if message.sender == user or message.recipient == user:
+                    conversation.append(message)
+            message_list.append(conversation)
+        context['contact_list'] = contact_list
+        context['message_list'] = message_list
         return context
