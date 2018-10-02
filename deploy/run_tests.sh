56a73907741ddde69b0639c54c53126c73c3de75
jtalowell <jtalowell@protonmail.com>
2018-10-02 11:39:32 +0000

    Updated server script with clearer headings

--- deploy/run_tests.sh
+++ deploy/run_tests.sh
@@ -7,6 +7,7 @@ DB_PW=$3
 for j in {1..4}; do
     for i in 10 100 1000 10000 100000 1000000; do
         echo Trial $j / Size $i >> test_output.txt 2>&1
+	echo '##################\n' >> test_output.txt 2>&1
 	BATCH_SIZE=$i SECRET_KEY=$SECRET_KEY DB_USER=$DB_USER DB_PW=$DB_PW ../manage.py test localhost --noinput >> test_output.txt 2>&1
     done
 done
