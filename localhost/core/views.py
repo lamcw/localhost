248a03aa9d7da6063a8d547b0ded2d570e472d09
Edward <eddai741@gmail.com>
2018-10-02 21:36:46 +1000

    Fixed bug with message ordering

--- localhost/core/views.py
+++ localhost/core/views.py
@@ -144,13 +144,12 @@ class MessagingView(ListView):
         message_list = []
         queryset = Message.objects.filter(
                 Q(sender=self.request.user.id) | Q(recipient=self.request.user.id))
-
-        for message in queryset:
+        for message in queryset.reverse():
             if message.sender != self.request.user and message.sender not in contact_list:
                 contact_list.append(message.sender)
             elif message.recipient != self.request.user and message.recipient not in contact_list:
                 contact_list.append(message.recipient)
-        contact_list.reverse()
+        print(contact_list)
         for user in contact_list:
             conversation = []
             for message in queryset:
