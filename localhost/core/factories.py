d7ee36ae72f767233131cfe4c96f4b9d82dec7a7
lamcw <thomas@lamcw.com>
2018-10-03 15:03:06 +1000

    Added factories for booking and property items.
    Signed-off-by: lamcw <thomas@lamcw.com>

--- localhost/core/factories.py
+++ localhost/core/factories.py
@@ -3,9 +3,12 @@ import datetime
 import factory
 from django.utils import timezone
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 from localhost.core import models
 from localhost.authentication.factories import UserFactory
+
+User = get_user_model()
 
 
 class PropertyItemFactory(factory.DjangoModelFactory):
@@ -40,3 +43,29 @@ class PropertyFactory(factory.DjangoModelFactory):
         'date_time_this_year', tzinfo=timezone.get_current_timezone())
     latest_checkin_time = factory.LazyAttribute(
         lambda o: o.earliest_checkin_time + datetime.timedelta(hours=5))
+
+
+class BookingFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = models.Booking
+
+    user = factory.Iterator(User.objects.all())
+    property_item = factory.Iterator(models.PropertyItem.objects.all())
+    price = 70
+    earliest_checkin_time = factory.Faker(
+        'date_time_between_dates',
+        datetime_start=timezone.make_aware(datetime.datetime(2018, 9, 1)),
+        datetime_end=timezone.make_aware(datetime.datetime(2018, 10, 10)),
+        tzinfo=timezone.get_current_timezone())
+    latest_checkin_time = factory.LazyAttribute(
+        lambda o: o.earliest_checkin_time + datetime.timedelta(hours=6))
+
+
+class PropertyItemReviewFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = models.PropertyItemReview
+
+    booking = factory.Iterator(models.Booking.objects.all())
+    rating = factory.Iterator([x for x in range(6)])
+    description = factory.Faker(
+        'paragraph', nb_sentences=3, variable_nb_sentences=True)
