42c3cb769537da58d6c7c7c42650458a6e28826c
Edward <eddai741@gmail.com>
2018-10-06 03:06:32 +1000

    subscribe to notifications inbox

--- localhost/core/consumers.py
+++ localhost/core/consumers.py
@@ -71,8 +71,11 @@ class BaseConsumer(MultiplexJsonWebsocketConsumer):
 
     def connect(self):
         user = self.scope.get('user')
+        notifications_id = 'notifications_' + str(user.id)
+        print(notifications_id)
         if user and user.is_authenticated:
             self.accept()
+            self.request_subscribe(notifications_id)
         else:
             self.close()
 
