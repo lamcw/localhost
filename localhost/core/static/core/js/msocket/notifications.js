4a5224fc7dac8b9164436f724879d79339ef4732
jtalowell <jtalowell@protonmail.com>
2018-10-13 02:10:27 +1100

    Adding empty string to notifications when empty

--- localhost/core/static/core/js/msocket/notifications.js
+++ localhost/core/static/core/js/msocket/notifications.js
@@ -11,6 +11,7 @@ function listen_notifications(csocket) {
       + data.url + '">' + data.message + '</a> <button id="button-notification-'
       + data.id + '" class="notification-btn btn btn-primary" style="color:black !important;">clear</button> </div>'
     );
+    check_empty_notifications();
   }
 
   csocket.register_handler('notification', notification_handler);
@@ -20,5 +21,13 @@ function listen_notifications(csocket) {
     var notification_id = notification.match(/\d+$/)[0];
     csocket.notification(notification_id, 'clear');
     $(this).parent().remove();
+    check_empty_notifications();
   });
+}
+
+function check_empty_notifications() {
+  var notif_dropdown = $('#notifications .dropdown-menu');
+  if (notif_dropdown.children().length == 0) {
+    $(notif_dropdown.append('<a id="notification-none" class="dropdown-item">None</a>'));
+  }
 }
