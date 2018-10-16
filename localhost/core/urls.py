679513269e5d71eb23f2fc6bba5b342f8ce7e044
jtalowell <jtalowell@protonmail.com>
2018-10-16 10:13:12 +0000

    Updating with fixes

--- localhost/core/urls.py
+++ localhost/core/urls.py
@@ -6,15 +6,17 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path(
-        '',
-        views.HomeView.as_view(),
-        name='home'),
+    path('', views.HomeView.as_view(), name='home'),
     path(
         'property/<int:pk>',
         views.PropertyDetailView.as_view(),
         name='property-detail'),
     path('search/', views.SearchResultsView.as_view(), name='search-results'),
     path('profile/<int:pk>', views.ProfileView.as_view(), name='profile'),
-    path('about', TemplateView.as_view(template_name='core/about_us.html'), name='about'),
+    path(
+        'about',
+        TemplateView.as_view(template_name='core/about_us.html'),
+        name='about'),
 ]
+
+handler404 = 'core.views.PageNotFoundView'
